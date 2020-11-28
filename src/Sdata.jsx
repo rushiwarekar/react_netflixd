01822cb88775cd20795b51701ff678fe8a23797f
rushikesh warekar <rushiwarekar@gmail.com>
2020-11-28 12:56:16 +0530

    reactNetflixd

---
+++ src/Sdata.jsx
@@ -0,0 +1,28 @@
+const Sdata =[
+    {
+        sname :"Stranger Things",
+        imgsrc:"https://wallpapercave.com/wp/wp1839578.jpg",
+        title :"NetFlix Original Series",
+        links :"https://www.netflix.com/in/title/80057281",
+    },    
+    {
+        sname :"DARK",
+        imgsrc:"https://wallpapercave.com/wp/wp4056410.jpg",
+        title :"NetFlix Original Series",
+        links :"https://www.netflix.com/in/title/80100172",
+    },
+    {
+        sname :"Extracurricular",
+        imgsrc:"https://wallpapercave.com/wp/wp7418473.jpg",
+        title :"NetFlix Original Series",
+        links :"https://www.netflix.com/in/title/80990668?source=35",
+    },
+    {
+        sname :"The Vampire Diaries",
+        imgsrc:"https://wallpapercave.com/wp/Sl98aK0.jpg",
+        title :"NetFlix Original Series",
+        links :"https://www.netflix.com/in/title/70143860",
+    },
+];
+
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "./Cards";
+import "./index.css";
+import Sdata from "./Sdata";
+
+const ncard = (val, index)=> {
+  return (
+    <Card
+      sname={val.sname}
+      imgsrc={val.imgsrc}
+      title={val.title}
+      link={val.links}
+    />
+  );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <>
+    <h1 className="heading_style">List of TOP NetFlix Orignal Series</h1>
+    {Sdata.map(ncard)} 
+  </>,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+/*
+function ncard(val){
+
+}
+//fat arrow function
+
+const ncard = (val)=> {
+
+}
+*/
